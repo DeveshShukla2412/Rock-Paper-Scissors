0a98a76cd451a4972e663e8ec74c647849617fa7
Devesh <devesh224.cavisson@gmail.com>
2020-11-19 12:33:26 +0530

    win logic

--- Desktop/Rock-Paper-Scissors/app.js
+++ Desktop/Rock-Paper-Scissors/app.js
@@ -1,9 +1,9 @@
-const userScore=0;
-const computerScore=0;
+let userScore=0;
+let computerScore=0;
 const userScore_span=document.getElementById("user_score");
 const computerScore_span=document.getElementById("comp_score");
 const scoreBoard_div=document.querySelector(".scoreboard");
-const result_div=document.querySelector(".result");
+const result_p=document.querySelector(".result > p");
 const rock_div=document.querySelector("#r");
 const paper_div=document.querySelector("#p");
 const scissors_div=document.querySelector("#s");
@@ -36,11 +36,57 @@ const randomNumber = Math.floor((Math.random() * 3 ));
 return choice[randomNumber];
 }
 
+function convertToWord(letter)
+{
+if(letter==="r") return "Rock";
+if(letter==="p") return "Paper";
+else "Scissors";
+
+}
+
+function win(userChoice,computerChoice)
+{
+userScore++;
+userScore_span.innerHTML=userScore;
+computerScore_span.innerHTML=computerScore;
+const smallUserWord = "user".fontsize(3).sub();
+const smallCompWord = "comp".fontsize(3).sub();
+result_p.innerHTML= `${convertToWord(userChoice)}${smallUserWord} Beats  ${convertToWord(computerChoice)}${smallCompWord}. You Win !`;
+}
+
+function lose(userChoice,computerChoice)
+{
+console.log("LOST");
+}
+
+function draw(userChoice,computerChoice)
+{
+console.log("DRAW");
+}
+
+
 getComputerChoice();
 
 function game(userChoice)
 {
 const computerChoice = getComputerChoice();
 
-
+switch (userChoice + computerChoice)
+{
+case "rs":
+case "pr":
+case "sp":
+win(userChoice,computerChoice);
+break;
+case "sr":
+case "rp":
+case "ps":
+lose(userChoice,computerChoice);
+break;
+case "rr":
+case "pp":
+case "ss":
+draw(userChoice,computerChoice);
+break;
+}
 }
